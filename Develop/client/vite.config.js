1d58ec1a3f76946aaba6a1db809223abea6e5a58
TivonaDe <153453135+TivonaDe@users.noreply.github.com>
2025-02-05 19:00:20 -0500

    Update vite.config.js

--- Develop/client/vite.config.js
+++ Develop/client/vite.config.js
@@ -3,11 +3,11 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 10000,
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:10001',
         changeOrigin: true,
         secure: false,
       },
