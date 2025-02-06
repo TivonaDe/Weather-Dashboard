d4b9930dd2fd78683317bf633ebfe9a83eeccff2
TivonaDe <153453135+TivonaDe@users.noreply.github.com>
2025-02-05 19:55:00 -0500

    Update server.ts

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -7,7 +7,7 @@ import routes from './routes/index.js';
 
 const app = express();
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 0.0.0.0;
 
 // Serve static files of entire client dist folder
 app.use(express.static('../client/dist'));
